5d94fe6b20ea23c75f7d3fafb7b9ba12ca74305c
IndyBob2019 <david.richardson2019@gmail.com>
2019-01-16 14:32:32 -0500

    breakpoint issue

--- src/modules/background.js
+++ src/modules/background.js
@@ -3,28 +3,34 @@ import tileData from '../data/tileData';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
 class Background extends Component {
-  constructor(props){
-    super(props);
-    this.state = {cols: 3};
-    this.checkBreakpoint = this.checkBreakpoint.bind(this);
-  }
-  componentDidMount() {
-    window.addEventListener("resize", this.checkBreakpoint());
-  }
-  checkBreakpoint(){
-    if(window.innerWidth >= 870 && this.state.cols !== 3){
-      this.setState({cols: 3});
-    } else if(window.innerWidth < 870 && this.state.cols === 3){
-      this.setState({cols: 2});
+  getCols(){
+    console.log("getCols");
+    console.log(isWidthUp('xs', window.innerWidth));
+    if(isWidthUp('xl', window.innerWidth)){
+      console.log('4');
+      return 4;
     }
-    console.log(this.state.cols)
+    else if(isWidthUp('md', window.innerWidth)){
+      console.log('3');
+      return 3;
+    }
+    else if(isWidthUp('sm', window.innerWidth)){
+      console.log('2');
+      return 2;
+    }
+    else if(isWidthUp('xs', window.innerWidth)){
+      console.log('1');
+      return 1;
+    }
+
   }
   render() {
     return (
       <div>
-        <GridList cellHeight={300} className="" cols={this.state.cols}>
+        <GridList cellHeight={300} className="" cols={this.getCols()}>
           {tileData.map(tile => (
             <GridListTile key={tile.img} cols={tile.cols || 1} rows={tile.rows}>
               <img src={tile.img} alt={tile.title} />
